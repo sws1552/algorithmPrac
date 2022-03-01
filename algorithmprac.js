f2a65c6981f1b83c4b470b64ee035f7a5882581f
sws1552 <sws1552@gmail.com>
2022-03-01 22:27:41 +0900

    ㅎㅎ

--- algorithmprac.js
+++ algorithmprac.js
@@ -1,10 +1,20 @@
+// const fs = require('fs'); 
+// const input = fs.readFileSync('/dev/stdin').toString() //.split('\n'); 
+// const input = fs.readFileSync('제출.txt').toString().split(' ');
+
+// let a = parseInt(input[0]);
+// let b = parseInt(input[1]);
+
+// console.log(a+b);
+
+
+
+
 const fs = require('fs'); 
 // const input = fs.readFileSync('/dev/stdin').toString() //.split('\n'); 
-const input = fs.readFileSync('제출.txt').toString().split(' ');
+const input = fs.readFileSync('제출.txt').toString().split('\r\n');
 
-// console.log(input);
 
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
+console.log(input);
 
-console.log(a+b);
+
